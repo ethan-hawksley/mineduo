61302c93b05f9dcc6013b62e30abf3706d217768
Ethan Hawksley <noreply@hawksley.dev>
2026-06-13 22:56:52 +0100

    feat: extract h1 to html dom

--- js/MenuHeader.js
+++ js/MenuHeader.js
@@ -2,14 +2,11 @@ export default class MenuHeader {
   constructor() {
     this.element = document.createElement('div');
 
-    // Create title for main menu
-    const title = document.createElement('h1');
-    title.textContent = 'MineDuo';
     // Add empty splash text to be filled later
     this.splashText = document.createElement('h2');
     this.splashText.id = 'splash-text';
 
-    this.element.append(title, this.splashText);
+    this.element.append(this.splashText);
 
     this.loadSplashTextAsync();
   }
